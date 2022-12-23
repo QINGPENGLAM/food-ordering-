49bca1cdf7249fabc5a507dbac05bc81cd6bad3d
QINGPENGLAM <qingpenglam@gmail.com>
2022-12-22 21:50:38 -0500

    transfer

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Cheeseburger',
+    category : 'american',
+    rating : 4.5,
+    price: 7,
+    img: 'images/american/cheeseburger.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Reuben sandwich',
+    category : 'american',
+    rating : 4.3,
+    price: 7,
+    img: 'images/american/Reuben sandwich.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Hot dogs',
+    category : 'american',
+    rating : 4.5,
+    price: 5,
+    img: 'images/american/Hot dogs.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Nachos',
+    category : 'american',
+    rating : 4.3,
+    price: 7,
+    img: 'images/american/Nachos.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Chicago-style pizza',
+    category : 'american',
+    rating : 4.3,
+    price: 10,
+    img: 'images/american/Chicago-style pizza.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Blueberry cobbler',
+    category : 'american',
+    rating : 4.3,
+    price: 5,
+    img: 'images/american/Blueberry cobbler.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Chocolate-chip cookies',
+    category : 'american',
+    rating : 4.3,
+    price: 3,
+    img: 'images/american/Chocolate-chip cookies.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Apple pie',
+    category : 'american',
+    rating : 4.3,
+    price: 5,
+    img: 'images/american/Apple pie.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Barbecue ribs',
+    category : 'american',
+    rating : 4.5,
+    price: 15,
+    img: 'images/american/Barbecue ribs.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Buta-No-Shogayaki (Ginger Pork)',
+    category : 'japan',
+    rating : 4.3,
+    price: 15,
+    img: 'images/japan/Buta-No-Shogayaki (Ginger Pork).jpg.webp',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Champon',
+    category : 'japan',
+    rating : 4.3,
+    price: 15,
+    img: 'images/japan/Champon.webp',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Edamame',
+    category : 'japan',
+    rating : 4.3,
+    price: 7,
+    img: 'images/japan/Edamame.webp',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Fugu',
+    category : 'japan',
+    rating : 4.5,
+    price: 15,
+    img: 'images/japan/Fugu.webp',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Gyoza',
+    category : 'japan',
+    rating : 4.3,
+    price: 7,
+    img: 'images/japan/Gyoza.webp',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Gyudon',
+    category : 'japan',
+    rating : 4.3,
+    price: 15,
+    img: 'images/japan/Gyudon.webp',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Thai Chicken Satay',
+    category : 'thai',
+    rating : 4.5,
+    price: 8,
+    img: 'images/thai/Thai Chicken Satay.webp',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Thai Mini Shrimp Lettuce Wraps',
+    category : 'thai',
+    rating : 4.3,
+    price: 10,
+    img: 'images/thai/Thai Mini Shrimp Lettuce Wraps.webp',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Tom Yum Soup With Coconut Milk (Tom Khaa)',
+    category : 'thai',
+    rating : 4.5,
+    price: 15,
+    img: 'images/thai/Tom Yum Soup With Coconut Milk (Tom Khaa).webp',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Larb Gai Thai Chicken Salad',
+    category : 'thai',
+    rating : 4.3,
+    price: 10,
+    img: 'images/thai/Larb Gai Thai Chicken Salad.webp  ',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Thai Waterfall Beef Salad',
+    category : 'thai',
+    rating : 4.5,
+    price: 12,
+    img: 'images/thai/Thai Waterfall Beef Salad.webp',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Green Papaya Salad (Som Tam)',
+    category : 'thai',
+    rating : 4.3,
+    price: 7,
+    img: 'images/thai/Green Papaya Salad (Som Tam).webp',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Thai Weeping Tiger Beef',
+    age: '32',
+    category : 'thai',
+    rating : 4.3,
+    price: 20,
+    img: 'images/thai/Thai Weeping Tiger Beef.webp',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Hangover stew (해장국)',
+    category : 'Korean',
+    rating : 4.5,
+    price: 15,
+    img: 'images/Korean/Hangover stew (해장국).jfif',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Kimchi (김치)',
+    category : 'Korean',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Korean/Kimchi (김치).jfif',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Soft Tofu Stew (순두부찌개)',
+    category : 'Korean',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Korean/Soft Tofu Stew (순두부찌개).webp',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Samgyeopsal (삼겹살)',
+    category : 'Korean',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Korean/Samgyeopsal (삼겹살).jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Tteokbokki (떡볶이)',
+    category : 'Korean',
+    rating : 4.5,
+    price: 7,
+    img: 'images/Korean/Tteokbokki (떡볶이).jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Jjajangmyeon (짜장면)',
+    category : 'Korean',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Korean/Jjajangmyeon (짜장면).jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Chimaek (치맥)',
+    category : 'Korean',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Korean/Chimaek (치맥).jfif',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Peking Roasted Duck',
+    category : 'chinese',
+    rating : 4.5,
+    price: 15,
+    img: 'images/chinese/Peking Roasted Duck.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Kung Pao Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chinese/Kung Pao Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Sweet and Sour Pork',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/Sweet and Sour Pork.webp',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Hot Pot',
+    category : 'chinese',
+    rating : 4.3,
+    price: 30,
+    img: 'images/chinese/Hot Pot.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Dumplings',
+    category : 'chinese',
+    rating : 4.5,
+    price: 8,
+    img: 'images/chinese/Dumplings.jpg  ',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Char Siu',
+    category : 'chinese',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chinese/Char Siu.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Ma Po Tofu',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/Ma Po Tofu.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Chilaquiles',
+    category : 'Mexico',
+    rating : 4.5,
+    price: 12,
+    img: 'images/Mexico/Chilaquiles.webp',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Huevos Rancheros (Ranch Eggs)',
+    category : 'Mexico',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Mexico/Huevos Rancheros (Ranch Eggs).webp',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Machaca (Shredded Dried Beef)',
+    category : 'Mexico',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Mexico/Machaca (Shredded Dried Beef).webp',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Discada (Plow disc BBQ)',
+    category : 'Mexico',
+    rating : 4.5,
+    price: 10,
+    img: 'images/Mexico/Discada (Plow disc BBQ).webp',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Enchiladas',
+    category : 'Mexico',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Mexico/Enchiladas.webp',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Tacos',
+    category : 'Mexico',
+    rating : 4.5,
+    price: 15,
+    img: 'images/Mexico/Tacos.webp',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Burritos',
+    category : 'Mexico',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Mexico/Burritos.webp',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Menudo (Pork Stew)',
+    category : 'Mexico',
+    rating : 4.5,
+    price: 20,
+    img: 'images/Mexico/Menudo (Pork Stew).webp',
+    quantity: 1
+},
+]
+
+export {foodItem};
